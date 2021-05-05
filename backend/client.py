f65d62d640dac9e5ffdc23e1fab3b10c3fe4a687
Ilja112358 <Null>
2021-05-05 19:49:35 +0300

    Backend init

---
+++ backend/client.py
@@ -0,0 +1,14 @@
+import grpc
+import datahash_pb2
+import datahash_pb2_grpc
+
+channel = grpc.insecure_channel('localhost:6066')
+stub = datahash_pb2_grpc.DataHashStub(channel)
+text = 'Scio me nihil scire'
+to_md5 = datahash_pb2.Text(data=text)
+response = stub.hash_md5(to_md5)
+print(response.data)
+to_sha256 = datahash_pb2.Text(data=text)
+response = stub.hash_sha256(to_sha256)
+print(response.data)
+
