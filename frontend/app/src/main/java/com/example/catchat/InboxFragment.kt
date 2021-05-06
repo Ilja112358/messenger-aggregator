014f7290aa93916d8ab252868c44d8607acfb07b
MhlvDenis <dns.aqua.ban@gmail.com>
2021-05-07 00:22:37 +0300

    first main commit

--- frontend/app/src/main/java/com/example/catchat/InboxFragment.kt
+++ frontend/app/src/main/java/com/example/catchat/InboxFragment.kt
@@ -11,7 +11,10 @@ class InboxFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inbox, container, false)
+    }
+
+    fun onClickSubmit(view: View) {
+
     }
 }
