d7dfde758190e9dffc2e1a0ef96c588d902de283
UltraPoc <34279993+UltraPoc@users.noreply.github.com>
2021-05-06 19:41:40 +0300

    spam

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,3 @@
-from flask import Flask, request
 from telethon import TelegramClient, events, sync
 from telethon.tl.types import InputMessagesFilterPhotos
 
@@ -6,12 +5,8 @@ from telethon.tl.types import InputMessagesFilterPhotos
 def out(username):
     api_id = 4139893
     api_hash = 'ece013c7c8869d3a584bb2cae5ae3fc7'
-
-app = Flask(__name__)
-@app.route('/api/tg/sendMessage', method='POST')
-def sendMessage():
-    if request.form['type'] == 'chat':
-        client_session = 'sessions/' + request.form['username']
-
-
-
+    client_session = 'sessions/' + username
+    global client
+    client = TelegramClient(client_session, api_id, api_hash)
+    client.start()
+out('Consumer')
--- backend/telethon_test.py
+++ backend/telethon_test.py
@@ -1,14 +1,18 @@
 from telethon import TelegramClient, events, sync
 from telethon.tl.types import InputMessagesFilterPhotos
+from time import sleep
 
 
 #user authorizathion
-phone = '+79992083343'
-api_id = 4139893
-api_hash = 'ece013c7c8869d3a584bb2cae5ae3fc7'
+phone = <your phone>
+api_id = <your_id>
+api_hash = <your hash>
 client = TelegramClient('sessions/Consumer', api_id, api_hash)
 client.start()
-
-test_user = '@durmankoo'
-client.send_message(test_user, 'Работать')
+dialogs = client.get_dialogs()
+#print(dialogs[12])
+chat = 'test'
+while 1:
+    client.send_message(595779751, 'ПРОГУЛОЧКУ БЫ')
+    sleep(2)
 #sent = client.send_code_request(phone)
