081d166dc091e7e2aa1ce03ab37ea020c2d976f0
MhlvDenis <dns.aqua.ban@gmail.com>
2021-05-06 23:15:39 +0300

    Update MainActivity.kt

--- frontend/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ frontend/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -14,10 +15,18 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+<<<<<<< Updated upstream
 import androidx.core.view.get
+=======
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+>>>>>>> Stashed changes
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.ui.gallery.GalleryFragment
+import com.example.myapplication.ui.home.HomeFragment
+import com.example.myapplication.ui.slideshow.SlideshowFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -45,6 +54,7 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+<<<<<<< Updated upstream
         navView.setItemIconTintList(null)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -64,6 +74,11 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+=======
+        navView.itemIconTintList = null
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+>>>>>>> Stashed changes
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -75,5 +90,27 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        var fragment: Fragment? = null
+
+        when (id) {
+            R.id.nav_home -> HomeFragment()
+            R.id.nav_gallery -> GalleryFragment()
+            R.id.nav_slideshow -> SlideshowFragment()
+        }
+
+
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.nav_host_fragment_container, fragment)
+            transaction.commit()
+        }
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
