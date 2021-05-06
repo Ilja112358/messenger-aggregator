e6a25090e36ea319e4a10a82b46d2ca473842889
Ilja112358 <Empty>
2021-05-06 11:47:22 +0300

    Update server grpc

--- backend/client.py
+++ backend/client.py
@@ -1,14 +1,10 @@
 import grpc
-import datahash_pb2
-import datahash_pb2_grpc
+import test_pb2
+import test_pb2_grpc
 
-channel = grpc.insecure_channel('84.252.137.106:6066')
-stub = datahash_pb2_grpc.DataHashStub(channel)
-text = 'Scio me nihil scire'
-to_md5 = datahash_pb2.Text(data=text)
-response = stub.hash_md5(to_md5)
-print(response.data)
-to_sha256 = datahash_pb2.Text(data=text)
-response = stub.hash_sha256(to_sha256)
+channel = grpc.insecure_channel('localhost:6066')
+stub = test_pb2_grpc.testStub(channel)
+empty = test_pb2.Empty()
+response = stub.get(empty)
 print(response.data)
 
