cda1bb7201e5bdd87d7986cd65dee6d43f371bb8
MhlvDenis <dns.aqua.ban@gmail.com>
2021-05-06 10:59:42 +0300

    Submit button

--- frontend/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ frontend/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -55,8 +55,4 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-    fun onClickSubmit(view: View) {
-
-    }
 }
--- frontend/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ frontend/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -32,9 +33,11 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
+        val submitView: Button = binding.submitButton
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
         return root
     }
 
