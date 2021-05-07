bd59d0b79cb9f892960709577e89011648b385a6
UltraPoc <34279993+UltraPoc@users.noreply.github.com>
2021-05-07 13:14:06 +0300

    tg api

--- backend/telethon_test.py
+++ backend/telethon_test.py
@@ -4,15 +4,19 @@ from time import sleep
 
 
 #user authorizathion
-phone = <your phone>
-api_id = <your_id>
-api_hash = <your hash>
-client = TelegramClient('sessions/Consumer', api_id, api_hash)
-client.start()
-dialogs = client.get_dialogs()
-#print(dialogs[12])
+phone = '+79213010190'
+api_id = 4139893
+api_hash = 'ece013c7c8869d3a584bb2cae5ae3fc7'
+client = TelegramClient('sessions/andrew', api_id, api_hash)
+code = 1234
+client.sign_in(phone, code)
+#client.start(phone)
+#dialogs = client.get_dialogs()
+#print(dialogs[3])
 chat = 'test'
-while 1:
-    client.send_message(595779751, 'ПРОГУЛОЧКУ БЫ')
+i = 0
+'''while i < 20:
+    client.send_message('@durmankoo', 'ПРОГУЛОЧКУ БЫ')
     sleep(2)
+    i += 1'''
 #sent = client.send_code_request(phone)
