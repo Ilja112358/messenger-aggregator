cd3f2ded6bea23f73b175118ce1c02e9d158f9db
MhlvDenis <dns.aqua.ban@gmail.com>
2021-05-07 12:36:28 +0300

    workable version

--- frontend/app/src/main/java/com/example/catchat/InboxFragment.kt
+++ frontend/app/src/main/java/com/example/catchat/InboxFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import com.example.myapplication.ui.home.Api
 
 class InboxFragment : Fragment() {
@@ -15,15 +16,12 @@ class InboxFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_inbox, container, false)
         val submitButton = view?.findViewById<Button>(R.id.submit_button)
+        val textView = view?.findViewById<TextView>(R.id.text_field)
 
         submitButton?.setOnClickListener {
-            println(Api().getHash("Are you Slava Marlow?\n"))
+            textView?.text = Api().getHash("Are you Slava Marlow?\n")
         }
 
         return view
-    }
-
-    fun onClickSubmit(view: View) {
-
     }
 }
