9d9c20e398cf8919bcf68c8ca17742e248ebe2e8
MhlvDenis <dns.aqua.ban@gmail.com>
2021-05-07 19:47:34 +0300

    testing

--- frontend/app/src/main/java/com/example/catchat/Extensions.kt
+++ frontend/app/src/main/java/com/example/catchat/Extensions.kt
@@ -1,9 +1,18 @@
 package com.example.catchat
 
+import android.app.Activity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
 
 fun View.hideKeyboard() {
     val inputMethodManager = context!!.getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as? InputMethodManager
     inputMethodManager?.hideSoftInputFromWindow(this.windowToken, 0)
+}
+
+fun Activity.hideSoftKeyboard() {
+    currentFocus?.let {
+        val inputMethodManager = ContextCompat.getSystemService(this, InputMethodManager::class.java)!!
+        inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+    }
 }
