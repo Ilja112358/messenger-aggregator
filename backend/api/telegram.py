e10e572dee72154955d2d8de177755293461a3fd
Ilja112358 <Empty>
2021-05-07 17:54:28 +0300

    Save our souls

--- backend/api/telegram.py
+++ backend/api/telegram.py
@@ -1,41 +1,30 @@
 from telethon import TelegramClient, events, sync
 import os
 from dotenv import load_dotenv
-# import tg_pb2_grpc
+from api.protobufs import tg_pb2_grpc
 
-load_dotenv('../.env')
+load_dotenv('.env')
 api_id = int(os.getenv('api_id'))
 api_hash = os.getenv('api_hash')
-phone = os.getenv('phone')
-uid = os.getenv('uid')
 
 
-class TgApi:
-    def auth(self, uid, phone, code=None, code_hash=None):
-        client = TelegramClient('tg_sessions/' + str(uid), api_id, api_hash)
+class TgApiServicer(tg_pb2_grpc.TgApiServicer):
+    def auth(self, request, context):
+        client = TelegramClient('tg_sessions/' + str(request.uid), api_id, api_hash)
         client.connect()
-        if code is None:
-            return client.send_code_request(phone)
+        if request.code is None:
+            return client.send_code_request(request.phone)
         else:
-            return client.sign_in(phone=phone, code=code, phone_code_hash=code_hash)
+            return client.sign_in(phone=request.phone, code=request.code, phone_code_hash=request.code_hash)
 
-    def get_messages(self):
+    def get_messages(self, request, context):
         pass
 
-    def send_message(self, uid, entity, message):
+    def send_message(self, request, context):
         client = TelegramClient('tg_sessions/' + str(uid), api_id, api_hash)
         client.connect()
-        if isinstance(message, str):
-            client.send_message(entity, message)
+        if isinstance(request.message, str):
+            client.send_message(request.entity, request.message)
 
     def logout(self):
         pass
-
-
-# auth(uid, phone)
-
-# auth(uid, phone, '<hash_code>')
-
-#client = TelegramClient('tg_sessions/' + uid, api_id, api_hash)
-#client.connect()
-#print(client.get_dialogs())
