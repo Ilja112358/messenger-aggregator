eb638c0d2ceae54ccb8b1f5308fe40ada115552e
UltraPoc <34279993+UltraPoc@users.noreply.github.com>
2021-05-07 21:55:51 +0300

    client upd

--- backend/client.py
+++ backend/client.py
@@ -4,6 +4,7 @@ from api.protobufs import tg_pb2_grpc
 
 channel = grpc.insecure_channel('localhost:6066')
 stub = tg_pb2_grpc.TgApiStub(channel)
-request = tg_pb2.User(uid='ilyich')
+request = tg_pb2.Send(uid='ilyich', entity='@durmankoo', message='Hello, Andrew!')
+'''request = tg_pb2.User(uid='ilyich')
 response = stub.get_dialogs(request)
-print(response)
+print(response)'''
