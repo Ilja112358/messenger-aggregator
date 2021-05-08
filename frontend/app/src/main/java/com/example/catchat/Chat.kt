ccd77b21ee6fbdda102b1c8181a8f7fa32e19003
Kirilenko Andrey <kirilenko.haa@gmail.com>
2021-05-08 16:01:58 +0300

    tabbar back

--- frontend/app/src/main/java/com/example/catchat/Chat.kt
+++ frontend/app/src/main/java/com/example/catchat/Chat.kt
@@ -1,12 +1,14 @@
 package com.example.catchat
 
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import java.util.*
 
 
@@ -22,7 +24,16 @@ class Chat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
-        initializeViews()
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = "Telegram"
+        toolbar.setBackgroundColor(Color.parseColor("#859D7D"))
+
+        setSupportActionBar(toolbar)
+
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar()?.setDisplayShowHomeEnabled(true);
+        getSupportActionBar()?.setHomeButtonEnabled(true)
+
 
         /*val chatPair: Array<String> = java.lang.ModuleLayer.Controller.getChatPair(this@Chat).split(
             PAIR_DELIMITER
@@ -46,6 +57,11 @@ class Chat : AppCompatActivity() {
                 }
             }
         })*/
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 
     private fun initSendButton(userId: String) {
