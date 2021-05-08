5ea6530244e2f382da61c583c6529dd9630ea253
durmanko <kott9radevelop@gmail.com>
2021-05-08 16:38:59 +0300

    Added mark read on messages

--- frontend/app/src/main/java/com/example/models/Dialog.kt
+++ frontend/app/src/main/java/com/example/models/Dialog.kt
@@ -1,3 +1,3 @@
 package com.example.models
 
-data class Dialog(val name: String, val lastMessage: String, val date: String)
+data class Dialog(val name: String, val lastMessage: String, val date: String, val unread_count : Int)
