04d1243a057f638ffb533890740729e3628390d2
Ilja112358 <Empty>
2021-05-08 22:40:26 +0300

    Upd gmail api

--- backend/client.py
+++ backend/client.py
@@ -4,9 +4,9 @@ from api.protobufs import common_pb2
 from api.protobufs import tg_pb2_grpc
 from api.protobufs import gmail_pb2_grpc
 
-test_number = 2
+test_number = 6
 
-channel = grpc.insecure_channel('84.252.137.106:6066')
+channel = grpc.insecure_channel('localhost:6066')
 tg_stub = tg_pb2_grpc.TgApiStub(channel)
 gmail_stub = gmail_pb2_grpc.GmailApiStub(channel)
 
@@ -26,9 +26,12 @@ elif test_number == 3:
     request = common_pb2.DialogRequest(uid='test', dialog_id=-595779751)
     response = tg_stub.mark_read(request)
 elif test_number == 4:
+    request = common_pb2.DialogRequest(uid='test', dialog_id=-595779751)
+    response = tg_stub.get_messages(request)
+elif test_number == 5:
     request = common_pb2.User(uid='test')
     response = gmail_stub.auth(request)
-elif test_number == 5:
+elif test_number == 6:
     request = common_pb2.DialogRequest(uid='test', thread_id='1794cd87dca5fff6')
     response = gmail_stub.get_messages(request)
 
