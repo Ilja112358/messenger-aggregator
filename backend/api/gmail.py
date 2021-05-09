269170cdce08242afa8e8a2c9ef25ec6ed12bd64
Ilja112358 <Empty>
2021-05-09 21:22:01 +0300

    Hack optimize

--- backend/api/gmail.py
+++ backend/api/gmail.py
@@ -36,8 +36,8 @@ class GmailApiServicer(gmail_pb2_grpc.GmailApiServicer):
                 creds = flow.run_local_server(port=0)
             with open('api/gmail_credentials/' + request.uid + '_token.json', 'w') as token:
                 token.write(creds.to_json())
-        response = common_pb2.StatusMessage(status='OK AND')
-        return response
+
+        return common_pb2.StatusMessage(status='OK AND')
 
     @gmail_auth
     def get_dialogs(self, request, context, service):
@@ -63,19 +63,19 @@ class GmailApiServicer(gmail_pb2_grpc.GmailApiServicer):
     def get_messages(self, request, context, service):
         me_email = service.users().getProfile(userId='me').execute().get('emailAddress')
 
-        thread = service.users().threads().get(userId='me', id=request.thread_id).execute().get('messages', [])
+        # Mini hack. Only snippets, haha
+        thread = service.users().threads() \
+            .get(userId='me', id=request.thread_id, format='metadata', metadataHeaders=['From']).execute() \
+            .get('messages', [])
         messages = []
         for message in thread:
             snippet = message.get('snippet')
             date = int(message.get('internalDate'))
-            name = ''
-            for header in message.get('payload').get('headers'):
-                if header.get('name') == 'From':
-                    name = 'me' if me_email in header.get('value') else header.get('value')
+            from_header = message.get('payload').get('headers')[0].get('value')
+            name = 'me' if me_email in from_header else from_header
             messages.append(common_pb2.Message(message=snippet, date=date, sender=name))
-        print(messages)
-        response = common_pb2.Messages(message=messages)
-        return response
+
+        return common_pb2.Messages(message=messages)
 
     @gmail_auth
     def send_message(self, request, context, service):
@@ -89,4 +89,5 @@ class GmailApiServicer(gmail_pb2_grpc.GmailApiServicer):
         smtp = 'To: ' + to + '\nSubject: ' + request.subject + '\n\n' + request.message
         raw = base64.b64encode(bytes(smtp, encoding='utf8')).decode('utf-8')
         service.users().messages().send(userId='me', body={'raw': raw, 'threadId': request.thread_id}).execute()
+
         return common_pb2.StatusMessage(status='OK AND')
