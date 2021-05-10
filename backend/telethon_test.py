7eb6132384c395385d8f6102878ea2879419b93a
UltraPoc <34279993+UltraPoc@users.noreply.github.com>
2021-05-10 04:24:57 +0300

    little fix

--- backend/telethon_test.py
+++ backend/telethon_test.py
@@ -5,11 +5,10 @@ from time import sleep
 
 
 #user authorizathion
-phone = '+79213010190'
-api_id = 4139893
-api_hash = 'ece013c7c8869d3a584bb2cae5ae3fc7'
+phone = '<your_phone>'
+api_id = <your_api_id>
+api_hash = '<your_api_hash>'
 client = TelegramClient('sessions/Andrew', api_id, api_hash)
-code = 1234
 #client.sign_in(phone, code)
 client.start()
 #dialogs = client.get_messages(423865152, 10)
@@ -27,7 +26,7 @@ i = 0
     if client.download_profile_photo(temp_dialog_id, 'avatars/' + str(temp_dialog_id) + '.jpg') == None:
         avatar_url = ''
     else:
-        avatar_url = 'http://84.252.137.106:6066/avatars/' + str(temp_dialog_id) + 'jpg'
+        avatar_url = 'http://yout_server' + str(temp_dialog_id) + 'jpg'
     print(avatar_url)'''
 '''temp_dialog_id = -595779751
 if os.path.exists('avatars/' + str(temp_dialog_id) + '.jpg'):
@@ -36,7 +35,7 @@ if os.path.exists('avatars/' + str(temp_dialog_id) + '.jpg'):
 if client.download_profile_photo(temp_dialog_id, 'avatars/' + str(temp_dialog_id) + '.jpg') == None:
     avatar_url = ''
 else:
-    avatar_url = 'http://84.252.137.106:6066/avatars/' + str(temp_dialog_id) + 'jpg'''''
+    avatar_url = 'http://your_server' + str(temp_dialog_id) + 'jpg'''''
 
 '''for dialog in dialogs:
     if i < 20:
@@ -90,35 +89,3 @@ for temp_message in temp_messages:
             sender = client.get_entity(temp_message.from_id.user_id).first_name + ' ' + client.get_entity(
                 temp_message.from_id.user_id).last_name
     print(sender)'''
-
-#print(client.get_entity(423865152).first_name + ' ' +client.get_entity(423865152).last_name)
-
-#print(dialogs[0].message)
-#dialogs = client.get_dialogs()
-#print(dialogs[1].message.from_id.user_id)
-i = 0
-#client.send_message(-465125280, 'test')
-#print(dialogs[i].name)
-#print(dialogs[i])
-
-
-
-#print(dialogs[i].message.peer_id.channel_id)
-#print(type(client.get_entity(dialogs[i])), dialogs[i].name)
-#print(dialogs[i].id, '', dialogs[i].name)
-
-#for dialog in dialogs:
- #   i += 1
-  #  print(dialog)
-#print(dialogs)
-#result = client(functions.messages.GetPeerDialogsRequest(peers=['username']))
-#print(result.dialogs[0].unread_count)
-
-
-#chat = 'test'
-#i = 0
-'''while i < 20:
-    client.send_message('@durmankoo', 'message')
-    sleep(2)
-    i += 1'''
-#sent = client.send_code_request(phone)
